fd36b3f224402806192028b6a51db91d3cb8f4ee
Yan Bentes <yanwiverson@gmail.com>
2020-06-04 19:15:03 -0300

    Cálculo de Frete

--- site.php
+++ site.php
@@ -68,7 +68,8 @@ $app->get("/cart", function() {
 
 	$page->setTpl("cart", [
 		"cart"=>$cart->getValues(),
-		"products"=>$cart->getProducts()
+		"products"=>$cart->getProducts(),
+		"error"=>Cart::getMsgError()
 	]);
 
 });
@@ -119,6 +120,17 @@ $app->get("/cart/:idroduct/remove", function($idproduct) {
 	$cart->removeProduct($product, true);
 
 	header("Location: /cart");
+	exit;
+
+});
+
+$app->post("/cart/freight", function() {
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST["zipcode"]);
+
+	header("Location:/cart");
 	exit;
 
 });
