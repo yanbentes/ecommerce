952caae035d0de3ca41fda5a59b8c68d12d74a69
Yan Bentes <yanwiverson@gmail.com>
2020-06-10 09:50:30 -0300

    Search and Pagination

--- admin-users.php
+++ admin-users.php
@@ -7,12 +7,40 @@ $app->get("/admin/users", function() {
 
 	User::verifyLogin();
 
-	$users = User::listAll();
+	$search = (isset($_GET["search"])) ? $_GET["search"] : "";
+
+	$page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1;
+
+	if($search != "") {
+
+		$pagination = User::getPageSearch($search, $page);
+
+	}
+	else {
+
+		$pagination = User::getPage($page, 1);
+
+	}
+
+	$pages = [];
+
+	for ($i=0; $i < $pagination["pages"] ; $i++) { 
+		
+		array_push($pages, [
+			"href"=>"/admin/users?" . http_build_query([
+				"page"=>$i+1,
+				"search"=>$search
+			]),
+			"text"=>$i+1
+		]);
+	}
 
 	$page = new PageAdmin();
 
 	$page->setTpl("users", array(
-		"users"=>$users
+		"users"=>$pagination["data"],
+		"search"=>$search,
+		"pages"=>$pages
 	));
 
 });
