4e5e9f5ae5010d4fe663be1fc51882587cf27ba8
Yan Bentes <yanwiverson@gmail.com>
2020-06-10 10:40:29 -0300

    Admin - Pedidos - Busca e Paginação

--- admin-orders.php
+++ admin-orders.php
@@ -90,10 +90,40 @@ $app->get("/admin/orders", function() {
 
 	User::verifyLogin();
 
+	$search = (isset($_GET["search"])) ? $_GET["search"] : "";
+
+	$page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1;
+
+	if($search != "") {
+
+		$pagination = Order::getPageSearch($search, $page);
+
+	}
+	else {
+
+		$pagination = Order::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($i=0; $i < $pagination["pages"] ; $i++) { 
+		
+		array_push($pages, [
+			"href"=>"/admin/orders?" . http_build_query([
+				"page"=>$i+1,
+				"search"=>$search
+			]),
+			"text"=>$i+1
+		]);
+	}
+
 	$page = new PageAdmin();
 
 	$page->setTpl("orders", [
-		"orders"=>Order::listAll()
+		"orders"=>$pagination["data"],
+		"search"=>$search,
+		"pages"=>$pages
 	]);
 
 })
